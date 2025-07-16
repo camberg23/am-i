19e6112647e682b86be0cc1a4f3548afe0effdd2
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2025-07-16 17:50:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ video_id = "zMRWe0h7KtY"
 st.markdown(f"""
 <div class="video-container">
     <iframe width="100%" height="400" 
-    src="https://www.youtube.com/embed/{video_id}?autoplay=1&mute=1" 
+    src="https://www.youtube.com/embed/{video_id}" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
     allowfullscreen>
